bf610bf23bf12330b5f7afafa66031714b274c3d
Paulo Castellano <paulofcastellano@gmail.com>
2021-01-22 23:03:23 -0300

    update zsh-syntax-highlighting

--- install.sh
+++ install.sh
@@ -51,8 +51,8 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 echo "Installing oh-my-zsh >> zsh-autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "Install zsh-syntax-highlighting..."
-apt install -y --force-yes zsh-syntax-highlighting
+echo "Installing oh-my-zsh >> zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "  Asdf requeriments..."
 apt install -y --force-yes dirmngr gpg
